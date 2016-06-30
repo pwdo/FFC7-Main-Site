05ce44428c06b4a6746ff267cef952165e9e7712
Aceler Chua <chakler@themissingbulb.com>
2016-06-30 13:34:41 +0800

    Changed nav to horizontal scrolling menu on mobile

--- js/dev/main.js
+++ js/dev/main.js
@@ -1,15 +1,15 @@
 // mobile menu patch
 
 // make svg logo clickable
-var logo = document.getElementById('logo'),
-    menu = document.getElementById('menu-label'),
-    nav  = document.getElementById('nav');
+// var logo = document.getElementById('logo'),
+//     menu = document.getElementById('menu-label'),
+//     nav  = document.getElementById('nav');
 
-menu.onclick = function() {
-    logo.classList.toggle('is-active');
-    nav.classList.toggle('is-active');
-}
-logo.onclick = function() {
-    logo.classList.toggle('is-active');
-    nav.classList.toggle('is-active');
-}
+// menu.onclick = function() {
+//     logo.classList.toggle('is-active');
+//     nav.classList.toggle('is-active');
+// }
+// logo.onclick = function() {
+//     logo.classList.toggle('is-active');
+//     nav.classList.toggle('is-active');
+// }
